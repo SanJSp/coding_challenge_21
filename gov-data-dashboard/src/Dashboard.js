28fbb00f6698e2046cfed2d3c445c27176da3bcc
Sandro Speh <sandro.speh@student.hpi.de>
2021-03-27 12:24:46 +0100

    final polish

--- gov-data-dashboard/src/Dashboard.js
+++ gov-data-dashboard/src/Dashboard.js
@@ -9,78 +9,9 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
-import Orders from './Orders';
 import DataTable from './Table';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const drawerWidth = 240;
-
 const useStyles = makeStyles((theme) => ({
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    boxSizing: 'border-box',
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
   appBarSpacer: theme.mixins.toolbar,
 }));
 
@@ -91,14 +22,14 @@ export default function Dashboard() {
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar
-        position="absolute"
+        position='absolute'
         className={clsx(classes.appBar, false && classes.appBarShift)}
       >
         <Toolbar className={classes.toolbar}>
           <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
+            component='h1'
+            variant='h6'
+            color='inherit'
             noWrap
             sx={{ flexGrow: 1 }}
           >
@@ -107,7 +38,7 @@ export default function Dashboard() {
         </Toolbar>
       </AppBar>
       <Box
-        component="main"
+        component='main'
         sx={{
           backgroundColor: (theme) =>
             theme.palette.type === 'light'
@@ -119,16 +50,14 @@ export default function Dashboard() {
         }}
       >
         <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
           <Grid container spacing={3}>
-            {/* Recent Orders */}
             <Grid item xs={12}>
               <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                 <DataTable />
               </Paper>
             </Grid>
           </Grid>
-          <Copyright sx={{ pt: 4 }} />
         </Container>
       </Box>
     </Box>
--- gov-data-dashboard/src/Table.js
+++ gov-data-dashboard/src/Table.js
@@ -5,99 +5,106 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
-import departments from './departments.json'
+import departments from './departments.json';
 
 class DataTable extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = 
-      {
-        data: [],
-      };
-    }
-
-    getDepartmentData(fetchedData){
-        const departmentsDescription = departments.departments
-        const regex =  /.*([Mm]inisterium).*/
-        let departmentNamesWithData = [];
-        let departmentsData = [];
-        
-        fetchedData.forEach(function(department){
-            if (department.display_name.match(regex)) {
-                departmentsData.push(department)
-                departmentNamesWithData.push(department.display_name)
-            }
-        })
-
-        departmentsData.forEach(department => {
-            const departmentDescription = departmentsDescription.find((entry) => entry.name === department.display_name  )
-            if(departmentDescription.subordinates){
-                departmentDescription.subordinates.forEach((subordinate => {
-                    const subordinateJson = fetchedData.find((entry) => entry.display_name === subordinate.name)
-                    department.package_count += subordinateJson.package_count 
-                }))
-            }
-        })
-        departmentsData = this.addDepartmentsWithoutData(departmentsData, departmentNamesWithData)
-        departmentsData.sort(function (a,b) {return b.package_count - a.package_count})
-
-        return departmentsData
-    }
-
-    addDepartmentsWithoutData(departmentsData, departmentNamesWithData){
-        const regex =  /.*([Mm]inisterium).*/
-        const listOfAllDepartments = []
-        departments.departments.forEach(function(department){
-            if (department.name.match(regex)) {
-                listOfAllDepartments.push(department.name)
-            }
-        })
-        const departmentsWithoutData = listOfAllDepartments.filter((item => !departmentNamesWithData.includes(item)))
-        console.log(departmentsWithoutData)
-
-        departmentsWithoutData.forEach(department => {
-            departmentsData.push({display_name: department, package_count: 0})
-        })
-
-        return departmentsData
-    }
-  
-    componentDidMount() {
-        fetch('https://www.govdata.de/ckan/api/3/action/organization_list?all_fields=true')
-        .then(response => response.json())
-        .then(data => { 
-            console.log(data.result)
-            const editedData = this.getDepartmentData(data.result)
-            this.setState({ data: editedData })}
-           );
-    }
-  
-    componentWillUnmount() {
-    }
-  
-    render() {
-        return (
-            <React.Fragment>
-              <Title>Anzahl der Datensätze pro Ministerium</Title>
-              <Table size="small">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Ministerium</TableCell>
-                    <TableCell align="right">Anzahl Datensätze</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {this.state.data.map((row) => (
-                    <TableRow key={row.display_name}>
-                      <TableCell>{row.display_name}</TableCell>
-                      <TableCell align="right">{row.package_count}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </React.Fragment>
-          );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
   }
 
-  export default DataTable
+  getDepartmentData(fetchedData) {
+    const departmentsDescription = departments.departments;
+    const regex = /.*([Mm]inisterium).*/;
+    let departmentNamesWithData = [];
+    let departmentsData = [];
+
+    fetchedData.forEach(function (department) {
+      if (department.display_name.match(regex)) {
+        departmentsData.push(department);
+        departmentNamesWithData.push(department.display_name);
+      }
+    });
+
+    departmentsData.forEach((department) => {
+      const departmentDescription = departmentsDescription.find(
+        (entry) => entry.name === department.display_name
+      );
+      if (departmentDescription.subordinates) {
+        departmentDescription.subordinates.forEach((subordinate) => {
+          const subordinateData = fetchedData.find(
+            (entry) => entry.display_name === subordinate.name
+          );
+          department.package_count += subordinateData.package_count;
+        });
+      }
+    });
+
+    departmentsData = this.addDepartmentsWithoutData(
+      departmentsData,
+      departmentNamesWithData
+    );
+    departmentsData.sort(function (a, b) {
+      return b.package_count - a.package_count;
+    });
+
+    return departmentsData;
+  }
+
+  addDepartmentsWithoutData(departmentsData, departmentNamesWithData) {
+    const regex = /.*([Mm]inisterium).*/;
+    const listOfAllDepartments = [];
+    departments.departments.forEach(function (department) {
+      if (department.name.match(regex)) {
+        listOfAllDepartments.push(department.name);
+      }
+    });
+    const departmentsWithoutData = listOfAllDepartments.filter(
+      (item) => !departmentNamesWithData.includes(item)
+    );
+
+    departmentsWithoutData.forEach((department) => {
+      departmentsData.push({ display_name: department, package_count: 0 });
+    });
+
+    return departmentsData;
+  }
+
+  componentDidMount() {
+    fetch('https://www.govdata.de/ckan/api/3/action/organization_list?all_fields=true')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.result);
+        const editedData = this.getDepartmentData(data.result);
+        this.setState({ data: editedData });
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Title>Anzahl der Datensätze pro Ministerium</Title>
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ministerium</TableCell>
+              <TableCell align='right'>Anzahl Datensätze</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.data.map((row) => (
+              <TableRow key={row.display_name}>
+                <TableCell>{row.display_name}</TableCell>
+                <TableCell align='right'>{row.package_count}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default DataTable;
